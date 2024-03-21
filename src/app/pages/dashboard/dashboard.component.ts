86da48df8dc30494e332de4ad56a237da19fda26
Mohammed amaan <amaanvisiontech@gmail.com>
2024-03-21 11:40:15 +0530

    footer part move to dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -6,7 +6,51 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  completedProjectsCount: number = 0;
+  runningProjectsCount:number=0;
+  membersCount: number = 0;
+  clientsCount: number = 0;
+  constructor(){}
   ngOnInit(): void {
+    this.incrementProjectCount();
+    this.incrementClientCount();
+    this.incrementMemberCount();
+    this.incrementClientsCount();
+  }
 
+  incrementProjectCount() {
+    this.completedProjectsCount++;
+    if(this.completedProjectsCount < 50){
+      setTimeout(() => {
+        this.incrementProjectCount();
+      }, 50);
+    }
+  }
+
+  incrementClientCount() {
+    this.runningProjectsCount++;
+    if(this.runningProjectsCount < 100){
+      setTimeout(() => {
+        this.incrementClientCount();
+      }, 50);
+    }
+  }
+
+  incrementMemberCount(){
+    this.membersCount++;
+    if(this.membersCount < 200){
+      setTimeout(() => {
+        this.incrementMemberCount();
+      }, 25);
+    }
+  }
+
+  incrementClientsCount(){
+    this.clientsCount++;
+    if(this.clientsCount < 200){
+      setTimeout(() => {
+        this.incrementClientsCount();
+      }, 25);
+    }
   }
 }
